cb7b36e4f21633a7109a186444acd25ad69d72e8
Michael <millerdev126@gmail.com>
2018-08-10 18:32:34 -0500

    Completed webscraper Script
    Finished webscraping functionality in the webscraping script.

--- webscraper/venv/Scripts/Webscraper.py
+++ webscraper/venv/Scripts/Webscraper.py
@@ -11,6 +11,33 @@ uClient.close()
 # Html parsing
 page_soup = soup(page_html, "html.parser")
 
-print(page_soup.h1)
+containers = page_soup.findAll("div", {"class": "item-container"})
+
+filename = "products.csv"
+f = open(filename, 'w')
+
+headers = "Brand, Product_Name, Shipping Costs\n"
+
+f.write(headers)
+
+
+for container in containers:
+    # grab brand name
+    brand = container.div.div.a.img["title"]
+    # grab item title
+    title_container = container.findAll("a", {"class": "item-title"})
+    product_name = title_container[0].text
+    # grab shipping costs
+    shipping_container = container.findAll("li", {"class": "price-ship"})
+    shipping = shipping_container[0].text.strip()
+
+    # print data so far
+    f.write(brand + "," + product_name.replace(",", "-") + "," + shipping + '\n')
+
+f.close()
+
+
+
+
 
 
